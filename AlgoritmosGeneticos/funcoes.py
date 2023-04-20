a3fbec694ac08b5db49ec098a22c7458ee41b7e5
Felipe dos Santos Minatogau <felipe220067@ilum.cnpem.br>
2023-04-20 15:58:29 -0300

    Atualizações nos experimentos para arrumar a conclusão.

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -4,27 +4,28 @@ import random
 #                                 EXPERIMENTO                                 #
 ###############################################################################
 
-def Himmelblau(x,y):
+
+def Himmelblau(x, y):
     """Função de Himmelblau, simplesmente.
-    
+
     Args:
         x: variável x
         y: variável y
-        
+
     Return:
         Cálculo da função para tais X e Y.
     """
-    
-    f = ((x**2 + y - 11)**2) + ((x + y**2 - 7)**2)
+
+    f = ((x**2 + y - 11) ** 2) + ((x + y**2 - 7) ** 2)
     return f
-    
+
 
 def funcao_objetivo_himmelblau(individuo):
     """Computa a função objetivo no problema de Himmelblau:
-    
+
     Args:
         individuo: lista contendo os genes das incógnitas a serem testadas
-    
+
     Return:
         Um valor representando o valor da função para tais genes X e Y.
     """
@@ -32,55 +33,57 @@ def funcao_objetivo_himmelblau(individuo):
     y = individuo[1]
     return Himmelblau(x, y)
 
+
 def gene_him():
-    """ Cria um float aleatório entre 0 e 10.
-    
+    """Cria um float aleatório entre 0 e 10.
+
     Args:
         N/A
-    
+
     Returns:
         Um float aleatório.
-    
+
     """
     z = random.random() * 10
     return z
-    
+
+
 def individuo_him(n_genes):
-    """ Cria um candidato para o problema de Himmelblau com dois genes.
-    
+    """Cria um candidato para o problema de Himmelblau com dois genes.
+
     Args:
-        n_genes: valor float aleatório de 0 a 10
-    
+        n_genes: quantidade de genes que terão. No problema de Himmelblau, sempre
+        deverá ser 2.
+
     Return:
         Candidato de coordenadas (x,y)
     """
-    
+
     individuo = []
-    
+
     for _ in range(n_genes):
-    
         v = gene_him()
         individuo.append(v)
-    
     return individuo
 
-def populacao_him(Tamanho,Numero):
-    """ Cria uma população para o problema de Himmelblau.
-    
+
+def populacao_him(Tamanho, Numero):
+    """Cria uma população para o problema de Himmelblau.
+
     Args:
         Tamanho: quantidade de indivíduos
         Número de Genes: Quantidade de genes em cada indivíduo.
-    
+
     Returns:
         População contendo a quantidade de indivíduos e o número de genes em cada um.
     """
-    
+
     populacao = []
-    
+
     for _ in range(Tamanho):
         populacao.append(individuo_him(Numero))
-    
     return populacao
+
 
 def selecao_torneio_min_him(populacao, fitness, tamanho_torneio=3):
     """Faz a seleção de uma população usando torneio.
@@ -90,7 +93,7 @@ def selecao_torneio_min_him(populacao, fitness, tamanho_torneio=3):
 
     Args:
       populacao: população do problema
-      fun_objetivo: função objetivo
+      fitness: função objetivo
       tamanho_torneio: quantidade de invidiuos que batalham entre si
 
     Returns:
@@ -101,10 +104,9 @@ def selecao_torneio_min_him(populacao, fitness, tamanho_torneio=3):
 
     # vamos fazer len(populacao) torneios! Que comecem os jogos!
     par_populacao_fitness = list(zip(populacao, fitness))
-    
-    #print(par_populacao_fitness)
 
-    
+    # print(par_populacao_fitness)
+
     for p in range(len(populacao)):
         combatentes = random.sample(par_populacao_fitness, tamanho_torneio)
 
@@ -112,17 +114,15 @@ def selecao_torneio_min_him(populacao, fitness, tamanho_torneio=3):
         minimo_fitness = float("inf")
 
         for par_individuo_fitness in combatentes:
-            
             individuo = par_individuo_fitness[0]
             fit = par_individuo_fitness[1]
             # queremos o individuo de menor fitness
             if fit < minimo_fitness:
                 selecionado = individuo
                 minimo_fitness = fit
-
         selecionados.append(selecionado)
+    return selecionados
 
-    return selecionados 
 
 def funcao_mutacao_him(individuo):
     """Troca o valor de variáveis dos indivíduos.
@@ -135,7 +135,8 @@ def funcao_mutacao_him(individuo):
     gene_a_ser_mutado = random.randint(0, len(individuo) - 1)
     individuo[gene_a_ser_mutado] = random.random() * 10
     return individuo
-    
+
+
 def cruzamento_him(pai, mae):
     """Operador de cruzamento de ponto simples.
     Args:
@@ -193,12 +194,12 @@ def cria_cidades(n):
 
     for i in range(n):
         cidades[f"Cidade {i}"] = (random.random(), random.random())
-
     return cidades
+
 
 def computa_mochila(individuo, objetos, ordem_dos_nomes):
     """Computa o valor total e peso total de uma mochila
-    
+
     Args:
       individiuo:
         Lista binária contendo a informação de quais objetos serão selecionados.
@@ -211,21 +212,18 @@ def computa_mochila(individuo, objetos, ordem_dos_nomes):
       valor_total: valor total dos itens da mochila em unidades de dinheiros.
       peso_total: peso total dos itens da mochila em unidades de massa.
     """
-    
+
     valor_total = 0
     peso_total = 0
-    
+
     for pegou_o_item_ou_nao, nome_do_item in zip(individuo, ordem_dos_nomes):
         if pegou_o_item_ou_nao == 1:
             valor_do_item = objetos[nome_do_item]["valor"]
             peso_do_item = objetos[nome_do_item]["peso"]
-            
+
             valor_total = valor_total + valor_do_item
             peso_total = peso_total + peso_do_item
-            
-        
     return valor_total, peso_total
-
 
 
 ###############################################################################
@@ -314,7 +312,6 @@ def individuo_senha(tamanho_senha, letras):
 
     for n in range(tamanho_senha):
         candidato.append(gene_letra(letras))
-
     return candidato
 
 
@@ -418,9 +415,7 @@ def selecao_roleta_max(populacao, fitness):
     Returns:
       População dos indivíduos selecionados.
     """
-    populacao_selecionada = random.choices(
-        populacao, weights=fitness, k=len(populacao)
-    )
+    populacao_selecionada = random.choices(populacao, weights=fitness, k=len(populacao))
     return populacao_selecionada
 
 
@@ -456,9 +451,7 @@ def selecao_torneio_min(populacao, fitness, tamanho_torneio=3):
             if fit < minimo_fitness:
                 selecionado = individuo
                 minimo_fitness = fit
-
         selecionados.append(selecionado)
-
     return selecionados
 
 
@@ -502,17 +495,15 @@ def cruzamento_ordenado(pai, mae):
     """
     corte1 = random.randint(0, len(pai) - 2)
     corte2 = random.randint(corte1 + 1, len(pai) - 1)
-    
+
     filho1 = pai[corte1:corte2]
     for gene in mae:
         if gene not in filho1:
             filho1.append(gene)
-            
     filho2 = mae[corte1:corte2]
     for gene in pai:
         if gene not in filho2:
             filho2.append(gene)
-            
     return filho1, filho2
 
 
@@ -571,20 +562,16 @@ def mutacao_de_troca(individuo):
       O indivíduo recebido como argumento, porém com dois dos seus genes
       trocados de posição.
     """
-    
+
     indices = list(range(len(individuo)))
     lista_sorteada = random.sample(indices, k=2)
-    
+
     indice1 = lista_sorteada[0]
     indice2 = lista_sorteada[1]
-    
+
     individuo[indice1], individuo[indice2] = individuo[indice2], individuo[indice1]
-    
+
     return individuo
-
-
-    
-    
 
 
 ###############################################################################
@@ -626,7 +613,6 @@ def funcao_objetivo_senha(individuo, senha_verdadeira):
 
     for letra_candidato, letra_oficial in zip(individuo, senha_verdadeira):
         diferenca = diferenca + abs(ord(letra_candidato) - ord(letra_oficial))
-
     return diferenca
 
 
@@ -648,27 +634,27 @@ def funcao_objetivo_cv(individuo, cidades):
     distancia = 0
 
     for posicao in range(len(individuo) - 1):
-        
         partida = cidades[individuo[posicao]]
         chegada = cidades[individuo[posicao + 1]]
-        
+
         percurso = distancia_entre_dois_pontos(partida, chegada)
-        distancia = distancia + percurso        
-               
+        distancia = distancia + percurso
     # Calculando o caminho de volta para a cidade inicial
     partida = cidades[individuo[-1]]
     chegada = cidades[individuo[0]]
 
     percurso = distancia_entre_dois_pontos(partida, chegada)
     distancia = distancia + percurso
-    
+
     return distancia
+
 
 # NOVIDADE
 
+
 def funcao_objetivo_mochila(individuo, objetos, limite, ordem_dos_nomes):
     """Computa a funcao objetivo de um candidato no problema da mochila.
-    
+
     Args:
       individiuo:
         Lista binária contendo a informação de quais objetos serão selecionados.
@@ -679,22 +665,19 @@ def funcao_objetivo_mochila(individuo, objetos, limite, ordem_dos_nomes):
         Número indicando o limite de peso que a mochila aguenta.
       ordem_dos_nomes:
         Lista contendo a ordem dos nomes dos objetos.
-        
+
     Returns:
       Valor total dos itens inseridos na mochila considerando a penalidade para
       quando o peso excede o limite.
     """
 
     valor_mochila, peso_mochila = computa_mochila(individuo, objetos, ordem_dos_nomes)
-    
+
     if peso_mochila > limite:
         valor_mochila = 0.01
-        
     return valor_mochila
 
     pass
-
-
 
 
 ###############################################################################
@@ -705,8 +688,10 @@ def funcao_objetivo_mochila(individuo, objetos, limite, ordem_dos_nomes):
 def funcao_objetivo_pop_cb(populacao):
     """Calcula a funcao objetivo para todos os membros de uma população
     Args:
+    
       populacao: lista com todos os individuos da população
     Return:
+    
       Lista de valores represestando a fitness de cada individuo da população.
     """
     fitness = []
@@ -718,10 +703,10 @@ def funcao_objetivo_pop_cb(populacao):
 
 def funcao_objetivo_pop_cnb(populacao):
     """Calcula a funcao objetivo para todos os membros de uma população
-    
+
     Args:
       populacao: lista com todos os individuos da população
-      
+
     Return:
       Lista de valores represestando a fitness de cada individuo da população.
     """
@@ -734,11 +719,11 @@ def funcao_objetivo_pop_cnb(populacao):
 
 def funcao_objetivo_pop_senha(populacao, senha_verdadeira):
     """Computa a funcao objetivo de uma populaçao no problema da senha.
-    
+
     Args:
       populacao: lista com todos os individuos da população
       senha_verdadeira: a senha que você está tentando descobrir
-      
+
     Returns:
       Lista contendo os valores da métrica de distância entre senhas.
     """
@@ -746,22 +731,21 @@ def funcao_objetivo_pop_senha(populacao, senha_verdadeira):
 
     for individuo in populacao:
         resultado.append(funcao_objetivo_senha(individuo, senha_verdadeira))
-
     return resultado
 
 
 # NOVIDADE
 def funcao_objetivo_pop_cv(populacao, cidades):
     """Computa a funcao objetivo de uma população no problema do caixeiro viajante.
-    
+
     Args:
       populacao:
         Lista com todos os inviduos da população
-        
+
       cidades:
         Dicionário onde as chaves são os nomes das cidades e os valores são as
         coordenadas das cidades.
-        
+
     Returns:
       Lista contendo a distância percorrida pelo caixeiro para todos os
       indivíduos da população.
@@ -771,6 +755,7 @@ def funcao_objetivo_pop_cv(populacao, cidades):
     for individuo in populacao:
         resultado.append(funcao_objetivo_cv(individuo, cidades))
     return resultado
+
 
 def funcao_objetivo_pop_mochila(populacao, objetos, limite, ordem_dos_nomes):
     """Computa a fun. objetivo de uma populacao no problema da mochila
@@ -791,9 +776,6 @@ def funcao_objetivo_pop_mochila(populacao, objetos, limite, ordem_dos_nomes):
     resultado = []
     for individuo in populacao:
         resultado.append(
-            funcao_objetivo_mochila(
-                individuo, objetos, limite, ordem_dos_nomes
-            )
+            funcao_objetivo_mochila(individuo, objetos, limite, ordem_dos_nomes)
         )
-
     return resultado
